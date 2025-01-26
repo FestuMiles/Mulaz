58f8e2d38d739821bd3deb69bef16a28430b67ba
FestuMiles <86722689+FestuMiles@users.noreply.github.com>
2025-01-26 17:50:45 +0200

    Added mongoose

--- app.js
+++ app.js
@@ -3,7 +3,46 @@ const ejs = require('ejs');
 const bodyParser = require('body-parser');
 //Importing an object from the data.js file
 const objects = require('./data.js');
+const mongoose = require('mongoose');
 
+//Connecting to the database
+mongoose.connect('mongodb://localhost:27017/furnitureDB');
+
+//Creating a schema
+const furnitureSchema = {
+  name: String,
+  price: Number,
+  priceTag: String,
+  imageUrl: String,
+  tag: String
+};
+
+
+
+//Creating a model
+const Furniture = mongoose.model('Furniture', furnitureSchema);
+
+const price = 300;
+
+// const furniture = new Furniture({
+//   name: 'Name of Furniture',
+//   price: price,
+//   priceTag: 'K' + price + '.00',
+//   imageUrl: 'images/1.jpg',
+//   tag: 'best-selling'
+// });
+// furniture.save();
+
+// objects.forEach((object) => {
+//   const furniture = new Furniture({
+//     name: object.name,
+//     price: object.price,
+//     priceTag: object.priceTag,
+//     imageUrl: object.imageUrl,
+//     tag: object.tag
+//   });
+//   furniture.save();
+// });
 
 const app = express();
 
@@ -12,10 +51,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('index', { bodycss: 'index.css', objects: objects });
+  Furniture.find({}).then((furniture) => {
+    res.render('index', { bodycss: 'index.css', objects: furniture });
+  }).catch((err) => {
+    console.log(err);
+  });
+  // res.render('index', { bodycss: 'index.css', objects: objects });
 });
 
 app.get('/explore', (req, res) => {
+  Furniture.find({}).then((furniture) => {
+    res.render('explore', { bodycss: 'explore.css', objects: furniture });
+  }).catch((err) => {
+    console.log(err);
+  });
     res.render('explore', { bodycss: 'explore.css' });
     }
 );

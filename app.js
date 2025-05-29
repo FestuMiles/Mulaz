8baad4130e67f77f072648a006adc1fc4e02227d
Festus Mwape <festusmwape2001@gmail.com>
2025-05-29 15:15:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -759,7 +759,7 @@ app.post('/submit-order', (req, res)=>{
 
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

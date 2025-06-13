922414161f3efa73c5b0f41830b4b9c1916f514e
Festus Mwape <festusmwape2001@gmail.com>
2025-06-14 01:00:41 +0200

    Configured Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./views/**/*.ejs'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'ui-sans-serif', 'system-ui']
+      }
+    }
+  }
+}
+// tailwind.config.js
+// This is a Tailwind CSS configuration file that sets up the content paths and extends the theme with custom font families.
